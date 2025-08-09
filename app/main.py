57834e55ddae7dc0a7f74e2826317f66f8fe7118
Oleksandr Skoromnov <solalex005@gmail.com>
2025-08-09 23:36:04 +0300

    fix2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from collections import namedtuple
-from collections.abc import Hashable
+from collections.abc import Hashable, Iterator
 from typing import Any
 
 Node = namedtuple("Node", ["key", "hash", "value"])
@@ -20,18 +20,25 @@ class Dictionary:
         while self.storage[index].key != key:
             index = (index + 1) % self.capacity
             if self.storage[index] is None:
-                return
-        self.storage[index] = None
+                raise KeyError("Missing key")
+        self.storage[index] = Node(key, Ellipsis, Ellipsis)
+        self.length -= 1
 
     def __getitem__(self, key: Hashable) -> Any:
         index = hash(key) % self.capacity
         current = self.storage[index]
         if current is None:
             raise KeyError("Missing key")
+        if self.storage[index].value is Ellipsis:
+            raise KeyError("Missing key")
         while self.storage[index].key != key:
             index = (index + 1) % self.capacity
             if self.storage[index] is None:
                 raise KeyError("Missing key")
+            if index == hash(key) % self.capacity:
+                raise KeyError("Missing key")
+        if self.storage[index].value is Ellipsis:
+            raise KeyError("Missing key")
         return self.storage[index].value
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
@@ -49,6 +56,10 @@ class Dictionary:
     def __set_item(self, node: Node, index: int) -> None:
         while True:
             if self.storage[index] is None:
+                self.storage[index] = node
+                self.length += 1
+                break
+            if self.storage[index].value is Ellipsis:
                 self.storage[index] = node
                 self.length += 1
                 break
@@ -86,17 +97,19 @@ class Dictionary:
             return default
 
     def pop(self, key: Any, default: Any = None) -> Any:
-        value = self.get(key, default)
-        if value == default:
-            raise KeyError("Missing key")
-        del self[key]
-        return value
+        try:
+            value = self[key]
+            del self[key]
+        except KeyError:
+            return default
+        else:
+            return value
 
     def update(self, data: dict[Any, Any] | Dictionary) -> None:
         for key in data:
             self[key] = data[key]
 
-    def __iter__(self) -> Dictionary:
+    def __iter__(self) -> Iterator[Hashable]:
         for node in self.storage:
             if node:
                 yield node.key
